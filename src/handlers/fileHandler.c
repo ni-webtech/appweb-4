1b2b2776980f673e1c4eafc3395a33d0b42ef40a
Embedthis Software <dev@embedthis.com>
2010-04-16 12:36:11 -0700

    Fix file handler bad assert

--- src/handlers/fileHandler.c
+++ src/handlers/fileHandler.c
@@ -217,7 +217,6 @@ static int readFileData(HttpQueue *q, HttpPacket *packet)
     conn = q->conn;
     trans = conn->transmitter;
     rec = conn->receiver;
-    mprAssert(len >= 0);
 
     if (packet->content == 0) {
         len = packet->entityLength;
