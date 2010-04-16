03843e98e7bcd35e0668c61641ec67b37a7b9e02
Embedthis Software <dev@embedthis.com>
2010-04-16 12:38:18 -0700

    Import mpr for fix to mprGetNormalizePath and sanitizeArgs

--- src/deps/mpr/mprLib.c
+++ src/deps/mpr/mprLib.c
@@ -4002,6 +4002,12 @@ void mprSetCmdDir(MprCmd *cmd, cchar *dir)
  */
 static int sanitizeArgs(MprCmd *cmd, int argc, char **argv, char **env)
 {
+#if VXWORKS
+    cmd->argv = argv;
+    cmd->argc = argc;
+    cmd->env = 0;
+#endif
+
 #if BLD_UNIX_LIKE
     char    *cp;
     int     index, i, hasPath, hasLibPath;
@@ -11385,8 +11391,8 @@ char *mprGetNormalizedPath(MprCtx ctx, cchar *pathArg)
     char            *dupPath, *path, *sp, *dp, *mark, **segments;
     int             addSep, i, segmentCount, hasDot, len, last, sep;
 
-    if (pathArg == 0 || pathArg == '\0') {
-        return mprStrdup(ctx, ".");
+    if (pathArg == 0 || *pathArg == '\0') {
+        return mprStrdup(ctx, "");
     }
     fs = mprLookupFileSystem(ctx, pathArg);
 
