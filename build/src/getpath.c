87c242c9fecaac6875e07100379b5eb22dc2c263
Embedthis Software <dev@embedthis.com>
2011-01-09 11:44:41 -0800

    First pass make package

--- build/src/getpath.c
+++ build/src/getpath.c
@@ -332,8 +332,8 @@ static char *canonPath(char *path)
 /*
  *  @copy   default
  *
- *  Copyright (c) Embedthis Software LLC, 2003-2011. All Rights Reserved.
- *  Copyright (c) Michael O'Brien, 1993-2011. All Rights Reserved.
+ *  Copyright (c) Embedthis Software LLC, 2003-2010. All Rights Reserved.
+ *  Copyright (c) Michael O'Brien, 1993-2010. All Rights Reserved.
  *
  *  This software is distributed under commercial and open source licenses.
  *  You may use the GPL open source license described below or you may acquire
@@ -356,11 +356,5 @@ static char *canonPath(char *path)
  *  for this software and support services are available from Embedthis
  *  Software at http://www.embedthis.com
  *
- *  Local variables:
-    tab-width: 4
-    c-basic-offset: 4
-    End:
-    vim: sw=4 ts=4 expandtab
-
-    @end
+ *  @end
  */
