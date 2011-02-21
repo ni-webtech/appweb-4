fc5770d3a9ecc66c8991dc10cec28a5df5cb16d1
Embedthis Software <dev@embedthis.com>
2011-02-20 17:00:01 -0800

    Fix searching for tools on windows. Path had /windows first

--- src/deps/mpr/mprLib.c
+++ src/deps/mpr/mprLib.c
@@ -1202,7 +1202,7 @@ static void sweep()
                 mprAssert(!IS_FREE(mp));
                 CHECK(mp);
                 BREAKPOINT(mp);
-                if (mgr) {
+                if (mgr && VALID_BLK(mp)) {
                     (mgr)(GET_PTR(mp), MPR_MANAGE_FREE);
                 }
             }
--- src/include/master/ejs.slots.h
+++ src/include/master/ejs.slots.h
@@ -303,8 +303,7 @@
 #define ES_XML                                                         129
 #define ES_XMLHttp                                                     130
 #define ES_XMLList                                                     131
-#define ES_XXXcmd                                                      132
-#define ES_global_NUM_CLASS_PROP                                       133
+#define ES_global_NUM_CLASS_PROP                                       132
 
 /*
    Prototype (instance) slots for "global" type 
@@ -369,8 +368,6 @@
 #define ES_encodeObjects_items                                         0
 #define ES_ejs_worker_postMessage_data                                 0
 #define ES_ejs_worker_postMessage_ports                                1
-#define ES_XXXcmd_cmdline                                              0
-#define ES_XXXcmd_data                                                 1
 
 
 /*
@@ -604,49 +601,27 @@
 /*
     Class property slots for the "Cmd" type 
  */
-#define ES_Cmd_XXXdaemon                                               0
-#define ES_Cmd_kill                                                    1
-#define ES_Cmd_XXXrun                                                  2
-#define ES_Cmd_XXXsh                                                   3
+#define ES_Cmd_sh                                                      0
+#define ES_Cmd_run                                                     1
+#define ES_Cmd_daemon                                                  2
+#define ES_Cmd_runx                                                    3
 #define ES_Cmd_NUM_CLASS_PROP                                          4
 
 /*
    Prototype (instance) slots for "Cmd" type 
  */
-#define ES_Cmd__response                                               0
-#define ES_Cmd_close                                                   1
-#define ES_Cmd_error                                                   2
-#define ES_Cmd_env                                                     3
-#define ES_Cmd_finalize                                                4
-#define ES_Cmd_flush                                                   5
-#define ES_Cmd_on                                                      6
-#define ES_Cmd_off                                                     7
-#define ES_Cmd_pid                                                     8
-#define ES_Cmd_read                                                    9
-#define ES_Cmd_readString                                              10
-#define ES_Cmd_readLines                                               11
-#define ES_Cmd_readXml                                                 12
-#define ES_Cmd_response                                                13
-#define ES_Cmd_start                                                   14
-#define ES_Cmd_status                                                  15
-#define ES_Cmd_stop                                                    16
-#define ES_Cmd_timeout                                                 17
-#define ES_Cmd_wait                                                    18
-#define ES_Cmd_write                                                   19
-#define ES_Cmd_XXXexec                                                 20
-#define ES_Cmd_NUM_INSTANCE_PROP                                       21
+#define ES_Cmd_NUM_INSTANCE_PROP                                       0
 #define ES_Cmd_NUM_INHERITED_PROP                                      0
 
 /*
     Local slots for methods in type "Cmd" 
  */
-#define ES_Cmd_XXXdaemon_cmdline                                       0
-#define ES_Cmd_kill_pid                                                0
-#define ES_Cmd_kill_signal                                             1
-#define ES_Cmd_XXXrun_cmdline                                          0
-#define ES_Cmd_XXXrun_data                                             1
-#define ES_Cmd_XXXsh_command                                           0
-#define ES_Cmd_XXXsh_data                                              1
+#define ES_Cmd_sh_cmdline                                              0
+#define ES_Cmd_sh_data                                                 1
+#define ES_Cmd_run_cmdline                                             0
+#define ES_Cmd_run_data                                                1
+#define ES_Cmd_daemon_cmdline                                          0
+#define ES_Cmd_runx_cmdline                                            0
 
 
 /*
@@ -2399,7 +2374,7 @@
 #define ES_XMLList_NUM_INSTANCE_PROP                                   20
 #define ES_XMLList_NUM_INHERITED_PROP                                  0
 
-#define _ES_CHECKSUM_ejs   1369601
+#define _ES_CHECKSUM_ejs   1337752
 
 #endif
 /************************************************************************/
