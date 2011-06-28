78c8b9e3f8a89d5106ddde6644fc5676ca84e729
Embedthis Software <dev@embedthis.com>
2011-06-27 22:49:59 -0700

    Fix Cmd.sanitize args

--- src/deps/mpr/mprLib.c
+++ src/deps/mpr/mprLib.c
@@ -5375,7 +5375,7 @@ static int sanitizeArgs(MprCmd *cmd, int argc, char **argv, char **env)
             }
             *dp++ = quote;
         } else {
-            strcpy(dp, qp);
+            strcpy(dp, cp);
             dp += strlen(cp);
         }
         if (*++ap) {
