27c2dd72b91bdb8435bd28d17f24be7dc845ae2f
Embedthis Software <dev@embedthis.com>
2011-10-26 10:48:03 -0700

    Fix finalizePhp when PHP not used

--- src/modules/phpHandler.c
+++ src/modules/phpHandler.c
@@ -498,21 +498,21 @@ static int finalizePhp(MprModule *mp)
     Http        *http;
     HttpStage   *stage;
 
-    TSRMLS_FETCH();
-
     if ((stage = httpLookupStage(MPR->httpService, "phpHandler")) == 0) {
         return 0;
     }
     if (stage->stageData == 0) {
         return 0;
     }
-
-    mprLog(4, "php: Finalize library before unloading");
-    php_module_shutdown(TSRMLS_C);
-    sapi_shutdown();
-    tsrm_shutdown();
-    http = MPR->httpService;
-    stage->stageData = 0;
+    {
+        TSRMLS_FETCH();
+        mprLog(4, "php: Finalize library before unloading");
+        php_module_shutdown(TSRMLS_C);
+        sapi_shutdown();
+        tsrm_shutdown();
+        http = MPR->httpService;
+        stage->stageData = 0;
+    }
     return 0;
 }
 
