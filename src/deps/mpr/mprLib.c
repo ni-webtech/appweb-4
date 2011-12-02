6d9b72d5d317183c43c5f463c32b75d0ca936acb
Embedthis Software <dev@embedthis.com>
2011-12-01 21:33:51 -0800

    Fix minThreads pool workers

--- src/deps/mpr/mprLib.c
+++ src/deps/mpr/mprLib.c
@@ -23203,6 +23203,7 @@ void mprSetMinWorkers(int n)
     ws = MPR->workerService;
     mprLock(ws->mutex);
     ws->minThreads = n; 
+    mprLog(4, "Pre-start %d workers", ws->minThreads);
     
     while (ws->numThreads < ws->minThreads) {
         worker = createWorker(ws, ws->stackSize);
@@ -23374,8 +23375,13 @@ static void pruneWorkers(MprWorkerService *ws, MprEvent *timer)
     if (mprGetDebugMode()) {
         return;
     }
+    mprLog(4, "Check to prune idle workers. Pool has %d workers. Limits %d-%d", 
+        ws->numThreads, ws->minThreads, ws->maxThreads);
     mprLock(ws->mutex);
     for (index = 0; index < ws->idleThreads->length; index++) {
+        if (ws->numThreads <= ws->minThreads) {
+            break;
+        }
         worker = mprGetItem(ws->idleThreads, index);
         if ((worker->lastActivity + MPR_TIMEOUT_WORKER) < MPR->eventService->now) {
             changeState(worker, MPR_WORKER_PRUNED);
@@ -23508,6 +23514,7 @@ static void workerMain(MprWorker *worker, MprThread *tp)
     worker->thread = 0;
     ws->numThreads--;
     mprUnlock(ws->mutex);
+    mprLog(4, "Worker exiting. There are %d workers remaining in the pool.", ws->numThreads);
 }
 
 
